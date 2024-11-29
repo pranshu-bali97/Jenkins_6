5f74dc5a6295417ec4e61e38207d8cbaac04ae75
pranshu-bali97 <pranshubali97@gmail.com>
2024-11-29 15:45:03 +0530

    saving

---
+++ jenkins-shared-library/vars/ansible.groovy
@@ -0,0 +1,9 @@
+def call() {
+    ansiblePlaybook(
+        credentialsId: config.credentialsId ?: '67566dc8-069e-4c59-8308-aac784619a7b',
+        installation: config.installation ?: 'Ansible',
+        inventory: config.inventory ?: '/jenkins-shared-library/resources/ansible/ansible.cfg',
+        playbook: config.playbook ?: '/jenkins-shared-library/resources/ansible/playbook.yml',
+        vaultTmpPath: config.vaultTmpPath ?: ''
+    )
+}

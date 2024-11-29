8a054728d22b8460fc29688ae37354a87b043042
pranshu-bali97 <105100319+pranshu-bali97@users.noreply.github.com>
2024-11-29 15:57:22 +0530

    Update ansible.groovy

--- jenkins-shared-library/vars/ansible.groovy
+++ jenkins-shared-library/vars/ansible.groovy
@@ -2,7 +2,7 @@ def call() {
     ansiblePlaybook(
         credentialsId: config.credentialsId ?: '67566dc8-069e-4c59-8308-aac784619a7b',
         installation: config.installation ?: 'Ansible',
-        inventory: config.inventory ?: '/jenkins-shared-library/resources/ansible/ansible.cfg',
+        inventory: config.inventory ?: '/jenkins-shared-library/resources/ansible/aws_ec2.yml',
         playbook: config.playbook ?: '/jenkins-shared-library/resources/ansible/playbook.yml',
         vaultTmpPath: config.vaultTmpPath ?: ''
     )
